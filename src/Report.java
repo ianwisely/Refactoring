c30146180f42426eb5023a389b96fd309b9d3df0
ianwisely <ian.wisely@gmail.com>
2014-02-21 21:09:25 +0000

    fixed issue with and refactored report

--- src/Report.java
+++ src/Report.java
@@ -5,7 +5,7 @@ public class Report {
 	public Report() {
 	}
 
-	Hashtable<Integer, ArrayList<String>> offeringToName = new Hashtable<Integer, ArrayList<String>>();
+	private Hashtable<Integer, ArrayList<String>> offerings = new Hashtable<Integer, ArrayList<String>>();
 
 	public void findStudentsAndTheirOfferings() throws Exception {
 		List<Schedule> schedules = SchedulePersistence.all();
@@ -17,30 +17,31 @@ public class Report {
 	}
 
 	private void populateStudentsAndTheirOfferingsList(Schedule schedule, Offering offering) {
-		ArrayList<String> list = (ArrayList<String>)offeringToName.get(new Integer(offering.getId()));
-		if (list == null) {
-			list = new ArrayList<String>();
-			offeringToName.put(new Integer(offering.getId()), list);
+		ArrayList<String> students = (ArrayList<String>)offerings.get(offering.getId());
+		if (students == null) {
+			students = new ArrayList<String>();
+			offerings.put(new Integer(offering.getId()), students);
 		}
-		list.add(schedule.getName());
+		students.add(schedule.getName());
 	}
 
-	public void addOfferingToReport(StringBuffer buffer, ArrayList<String> list, Offering offering) {
+	public void addOfferingToReport(StringBuffer buffer, ArrayList<String> students, Offering offering) {
 		buffer.append(offering.getCourse().getName() + " " + offering.getDaysTimes() + "\n");
-		for (String string : list) {
+		for (String string : students) {
 			buffer.append("\t" + string + "\n");
 		}
 	}
 
 	public void generateReport(StringBuffer buffer) throws Exception {
 		findStudentsAndTheirOfferings();
-		ArrayList<Integer> offeringIds = (ArrayList<Integer>)offeringToName.keys();
-		for (Integer offeringId : offeringIds) {
-			ArrayList<String> list = (ArrayList<String>)offeringToName.get(offeringId);
-			addOfferingToReport(buffer, list, OfferingPersistence.find(offeringId.intValue()));
+		Enumeration<Integer> enumeration  = (Enumeration<Integer>)offerings.keys();
+		while (enumeration.hasMoreElements()) {
+			Integer offeringId = (Integer) enumeration.nextElement();
+			ArrayList<String> students = (ArrayList<String>)offerings.get(offeringId);
+			addOfferingToReport(buffer, students, OfferingPersistence.find(offeringId.intValue()));
 		}
 		buffer.append("Number of scheduled offerings: ");
-		buffer.append(offeringToName.size());
+		buffer.append(offerings.size());
 		buffer.append("\n");
 	}
 }
